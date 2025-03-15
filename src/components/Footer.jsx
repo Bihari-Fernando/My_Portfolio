e3f3371c6c93d7cf20894537adf8e75e05280642
Bihari-Fernando <biharifernando2000@gmail.com>
2025-03-15 10:00:05 +0530

    removed border from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='max-w=[1200px] sm:h-[100px] border border-gray-600 p-10 flex justify-between mx-auto'>
+    <div className='max-w=[1200px] sm:h-[100px] p-10 flex justify-between mx-auto'>
         <span className='primary-color'>BIHARI FERNANDO</span>
         <p className='text-gray-400'>biharifernando00@gmail.com <br/> #webDev2025</p>
     </div>
