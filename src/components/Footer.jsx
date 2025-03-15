24ea046331a1584e3d5d8876f18354a3087d0fe8
Bihari-Fernando <biharifernando2000@gmail.com>
2025-03-15 09:51:50 +0530

    created footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-w=[1200px] sm:h-[100px] border border-gray-600 p-10 flex justify-between mx-auto'>
+        <span className='primary-color'>BIHARI FERNANDO</span>
+        <p className='text-gray-400'>biharifernando00@gmail.com <br/> #webDev2025</p>
+    </div>
+  )
+}
+
+export default Footer
