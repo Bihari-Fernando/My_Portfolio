06b9b4389241f36fffb1ef9cee6d01717e5f874a
Bihari-Fernando <biharifernando2000@gmail.com>
2025-03-19 14:18:40 +0530

    enabled speed insights with vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Skills from './components/Skills'
 import Work from './components/Work'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
 
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Skills/>
-      <About/>
-      <Work/>
-      <Contact/>
-      <Footer/>
+      <SpeedInsights />
+      <Navbar />
+      <Hero />
+      <Skills />
+      <About />
+      <Work />
+      <Contact />
+      <Footer />
     </>
   )
 }
